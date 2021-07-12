6a015f361dd77ca28488a54c0acd721c1b18c3fb
Ross Riley <riley.ross@gmail.com>
2021-07-12 19:30:51 +0800

    fix

--- src/Storage/Schema/Table/Account.php
+++ src/Storage/Schema/Table/Account.php
@@ -28,7 +28,7 @@ class Account extends BaseTable
         $this->table->addColumn('enabled',     'boolean',    ['default' => 0]);
         $this->table->addColumn('verified',    'boolean',    ['default' => 0]);
         $this->table->addColumn('roles',       'json_array', []);
-        $this->table->addColumn('created_at',  "timestamp", ['datetime' => 'NOW()']);
+        $this->table->addColumn('created_at',  "timestamp", ['datetime' => 'CURRENT_TIMESTAMP']);
     }
 
     /**
